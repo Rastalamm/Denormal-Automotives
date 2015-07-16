0e6760aa5c4246d8c34e20fe920c629b7752d792
Judah Lamm <Judah.Lamm@gmail.com>
2015-07-15 21:50:35 -1000

    need to insert values into the all_car_models table

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,2 +1,49 @@
 CREATE USER normal_user;
 CREATE DATABASE normal_cars OWNER normal_user ENCODING 'utf8';
+
+CREATE TABLE IF NOT EXISTS car_companies
+(
+id serial,
+make_code character varying(125) NOT NULL,
+make_title character varying(125) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS release_year
+(
+id serial,
+year integer NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS all_car_models
+(
+id serial,
+model_code character varying(125) NOT NULL,
+model_title character varying(125) NOT NULL,
+
+car_companies_id integer NOT NULL,
+release_year_id integer NOT NULL,
+PRIMARY KEY (id),
+
+CONSTRAINT car_companies_id_fk FOREIGN KEY (car_companies_id)
+REFERENCES car_companies (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE,
+
+CONSTRAINT release_year_id_fk FOREIGN KEY (release_year_id)
+REFERENCES release_year (id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+--adds rows to the car_companies
+INSERT INTO car_companies (make_code, make_title)
+SELECT DISTINCT make_code, make_title
+  FROM car_models;
+
+--adds rows to the release_year
+INSERT INTO release_year (year)
+SELECT DISTINCT year
+  FROM car_models;
+
+--adds rows to the all_car_models
+INSERT INTO all_car_models (model_code, model_title)
+SELECT DISTINCT model_code, model_title
+  FROM car_models;
